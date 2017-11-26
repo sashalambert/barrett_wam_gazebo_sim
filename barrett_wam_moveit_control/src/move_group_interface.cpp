7a82f01a8cb040266ecf77c63dc641635a11c915
sasha <lambert.sasha@gmail.com>
2017-11-26 16:55:54 -0500

    Cartesian path following (position only).

--- barrett_wam_moveit_control/src/move_group_interface.cpp
+++ barrett_wam_moveit_control/src/move_group_interface.cpp
@@ -1,3 +1,5 @@
+#include <math.h>
+
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 
@@ -8,6 +10,9 @@
 #include <moveit_msgs/CollisionObject.h>
 
 #include <moveit_visual_tools/moveit_visual_tools.h>
+
+#define CARTESIAN
+// #define RANDOM
 
 int main(int argc, char **argv)
 {
@@ -27,11 +32,109 @@ int main(int argc, char **argv)
   ROS_INFO_NAMED("move_group_interface", "Reference frame: %s", move_group.getPlanningFrame().c_str());
   ROS_INFO_NAMED("move_group_interface", "End effector link: %s", move_group.getEndEffectorLink().c_str());
 
-
   moveit::planning_interface::MoveGroupInterface::Plan my_plan;
 
+  /*----------------------------------------------------------------------------------------------*/
+  // MOVE TO HOME POSITION
+
+  moveit::core::RobotStatePtr current_state = move_group.getCurrentState();
+  std::vector<double> joint_group_positions;
+  current_state->copyJointGroupPositions(joint_model_group, joint_group_positions);
+  joint_group_positions[0] = 0.0;
+  joint_group_positions[1] = -M_PI/2;
+  joint_group_positions[2] = 0;
+  joint_group_positions[3] = M_PI/2; 
+  joint_group_positions[4] = 0.0;
+  joint_group_positions[5] = 0.0;
+  joint_group_positions[6] = 0.0;
+
+  // move_group.setMaxVelocityScalingFactor(0.1);
+
+  move_group.setJointValueTarget(joint_group_positions);
   bool success = move_group.plan(my_plan);
   move_group.execute(my_plan);
+
+  // ros::Duration(5.0).sleep();
+
+#ifdef CARTESIAN
+  /*----------------------------------------------------------------------------------------------*/
+  // FOLLOW CARTESIAN WAYPOINTS
+
+  // The package MoveItVisualTools provides many capabilties for visualizing objects, robots,
+  // and trajectories in Rviz as well as debugging tools such as step-by-step introspection of a script
+  namespace rvt = rviz_visual_tools;
+  moveit_visual_tools::MoveItVisualTools visual_tools("odom_combined");
+  visual_tools.deleteAllMarkers();
+
+  // Current end-eff pose
+  geometry_msgs::Pose current_pose = move_group.getCurrentPose().pose;
+
+  std::vector<geometry_msgs::Pose> waypoints;
+  waypoints.push_back(current_pose);
+
+  double dist=0.1;
+
+  geometry_msgs::Pose target_pose = current_pose;
+  target_pose.position.x += dist;
+  waypoints.push_back(target_pose);  
+
+  target_pose.position.x -= dist;
+  target_pose.position.y += dist;
+  waypoints.push_back(target_pose); 
+
+  target_pose.position.y -= dist;
+  target_pose.position.x -= dist;
+  waypoints.push_back(target_pose); 
+
+  target_pose.position.x += dist;
+  target_pose.position.y -= dist;
+  waypoints.push_back(target_pose); 
+
+  target_pose.position.y += dist;
+  waypoints.push_back(target_pose); 
+
+
+  // Cartesian motions are frequently needed to be slower for actions such as approach and retreat
+  // grasp motions. Here we demonstrate how to reduce the speed of the robot arm via a scaling factor
+  // of the maxiumum speed of each joint. Note this is not the speed of the end effector point.
+  move_group.setMaxVelocityScalingFactor(0.1);
+
+
+    // We want the cartesian path to be interpolated at a resolution of 1 cm
+  // which is why we will specify 0.01 as the max step in cartesian
+  // translation.  We will specify the jump threshold as 0.0, effectively disabling it.
+  // Warning - disabling the jump threshold while operating real hardware can cause
+  // large unpredictable motions of redundant joints and could be a safety issue
+  moveit_msgs::RobotTrajectory trajectory;
+  const double jump_threshold = 0.0;
+  const double eef_step = 0.01;
+  double fraction = move_group.computeCartesianPath(waypoints, eef_step, jump_threshold, trajectory);
+  ROS_INFO_NAMED("tutorial", "Visualizing plan (cartesian path) (%.2f%% acheived)", fraction * 100.0);
+
+
+  // Visualize the plan in Rviz
+  visual_tools.deleteAllMarkers();
+  visual_tools.publishPath(waypoints, rvt::LIME_GREEN, rvt::SMALL);
+  for (std::size_t i = 0; i < waypoints.size(); ++i)
+    visual_tools.publishAxisLabeled(waypoints[i], "pt" + std::to_string(i), rvt::SMALL);
+  visual_tools.trigger();
+
+  moveit::planning_interface::MoveGroupInterface::Plan cartesian_plan;
+  cartesian_plan.trajectory_ = trajectory;
+  move_group.execute(cartesian_plan);
+  ros::Duration(3.0).sleep();
+
+#endif
+
+#ifdef RANDOM
+
+  /*----------------------------------------------------------------------------------------------*/
+  // GENERATE RANDOM START/GOAL PLANS
+
+  // moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+
+  // bool success = move_group.plan(my_plan);
+  // move_group.execute(my_plan);
 
   int iter=0;
   double count=0;
@@ -60,5 +163,7 @@ int main(int argc, char **argv)
   double stat= count / (double) iter;
 
   ROS_INFO("Avg. Exec. time:  %10.6f sec", stat);
+
+#endif	
 
 }
